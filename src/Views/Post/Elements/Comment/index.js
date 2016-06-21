4f4512e0997e7a4db71cf983b390c208a8bdc607
cherishhsieh <xieqin1991@gmail.com>
2016-06-21 15:31:04 +0800

    add post component

--- src/Views/Post/Elements/Comment/index.js
+++ src/Views/Post/Elements/Comment/index.js
@@ -2,7 +2,8 @@ import React, {
   StyleSheet,
   View,
   Text,
-  TouchableHighlight
+  TouchableHighlight,
+  Component
 } from 'react-native'
 import api from '../../../../NetWork/api'
 
@@ -34,4 +35,145 @@ class Comment extends Component {
       </View>
     )
   }
+
+  getRepliesControlButtonText () {
+    if(!this.state.subCommentsHidden){
+        return(
+          <Text style={styles.showRepliesButtonText}>
+            Hide Replies
+          </Text>
+        )
+      }
+      else{
+        return(
+          <Text style={styles.showRepliesButtonText}>
+            Show Replies ({this.props.data.kids.length})
+          </Text>
+        )
+      }
+    }
+
+  renderRepliesControlButton () {
+    if(this.state.subCommentsLoading){
+      return(
+        <Text style={styles.showRepliesButtonText}>
+          Loading Replies...
+        </Text>
+      )
+    }
+    if(this.props.data.kids){
+      return(
+        <TouchableHighlight onPress={()=>this.onShowReplies()}
+                  style={styles.showRepliesButton}
+                  underlayColor='#F6F6EF'>
+        {this.getRepliesControlButtonText()}
+      </TouchableHighlight>
+    )
+    }
+    else{
+      return(<View />)
+    }
+  }
+
+  renderSubComments () {
+    if(!this.state.subCommentsData || this.state.subCommentsHidden){
+      return(<View/>)
+    }
+    return(
+      <View>
+        {this.state.subCommentsData.map(subComment => <Comment data={subComment} level={this.state.level+1} key={subComment.count}/>)}
+      </View>
+    );
+  }
+
+  onShowReplies () {
+    if(this.state.subCommentsData && this.state.subCommentsHidden){
+      this.setState({
+        subCommentsHidden: false
+      })
+    }
+    else if(this.state.subCommentsData && !this.state.subCommentsHidden){
+      this.setState({
+        subCommentsHidden: true
+      })
+    }
+    else{
+      this.setState({
+        subCommentsLoading: true
+      });
+      var subCommentIDs = this.props.data.kids
+      var subCommentsData = []
+      var index = 0
+      var _this = this
+      function iterateAndFetch(){
+          if (index < subCommentIDs.length){
+              fetch(api.HN_ITEM_ENDPOINT+subCommentIDs[index]+".json")
+              .then((response) => response.json())
+              .then((item) => {
+                  item.count = index+1
+                      if(!item.deleted){
+                    subCommentsData.push(item)
+                      }
+                  index++
+                  iterateAndFetch()
+              })
+              .done()
+          }
+          else {
+              _this.setState({
+                subCommentsLoading: false,
+                subCommentsHidden: false,
+                subCommentsData: subCommentsData
+              });
+              return;
+          }
+      }
+      iterateAndFetch()
+    }
+  }
+  fixCommentText (str) {
+    return String(str).replace(/<p>/g, '\n\n')
+                .replace(/&#x2F;/g, '/')
+                .replace('<i>', '')
+                .replace('</i>', '')
+                .replace(/&#x27;/g, '\'')
+                .replace(/&quot;/g, '\"')
+                .replace(/<a\s+(?:[^>]*?\s+)?href="([^"]*)" rel="nofollow">(.*)?<\/a>/g, "$1");
+  }
 }
+
+const styles = StyleSheet.create({
+  commentInnerContainer: {
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    borderRadius: 5,
+    marginRight:10,
+    marginLeft:10,
+    marginTop:10,
+    padding:10,
+  },
+  commentOuterContainer: {
+
+  },
+  commentBy: {
+    fontSize: 13,
+    marginBottom: 3,
+    textAlign: 'left',
+    color: '#FF6600'
+  },
+  commentText: {
+    fontSize: 13,
+    textAlign: 'left',
+    color: '#000000'
+  },
+  showRepliesButtonText:{
+    marginTop: 10,
+    fontSize: 13,
+    textAlign: 'right',
+    color: '#FF6600',
+    textAlign: 'right',
+  }
+})
+
+export default Comment
--- src/Views/Post/index.android.js
+++ src/Views/Post/index.android.js
@@ -6,53 +6,189 @@ import React, {
   Platform,
   Component
 } from 'react-native'
-import GiftedListView from 'react-native-gifted-listview'
+import ToolbarAndroid from 'ToolbarAndroid'
+import api from '../../NetWork/api'
+import RefreshableListView from '../../Components/RefreshableListView'
+import Comment from './Elements/Comment'
 
 class Post extends Component {
-  getInitialState () {
-    return {
-      renderRow: this.props.renderRow,
-      backgroundColor: this.props.backgroundColor ? this.props.backgroundColor : '#fff',
-      loadMoreText: this.props.loadMoreText ? this.props.loadMoreText : 'Load More...',
-      renderHeader: this.renderHeader ? this.props.renderHeader : null,
+  render() {
+    return (
+      <View style={styles.container}>
+         <ToolbarAndroid style={styles.toolbar}
+                          title={this.props.title}
+                          navIcon={{uri: "ic_arrow_back_white_24dp", isStatic: true}}
+                          onIconClicked={this.props.navigator.pop}
+                          titleColor={'#FFFFFF'}/>
+          <RefreshableListView renderRow={(row)=>this.renderListViewRow(row)}
+                               renderHeader={this.renderListViewHeader}
+                               onRefresh={(page, callback)=>this.listViewOnRefresh(page, callback)}
+                               backgroundColor={'#F6F6EF'}
+                               loadMoreText={'Load More...'}/>
+      </View>
+    )
+  }
+
+  renderListViewRow (row) {
+    if(row.count==this.props.post.kids.length){
+      return(
+        <View style={{paddingBottom: 20}}>
+          <Comment data={row}/>
+        </View>
+      )
+    }
+    return(
+      <Comment data={row}/>
+    )
+  }
+
+  renderListViewHeader () {
+    return(
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>
+          {this.props.post.title}
+        </Text>
+        {this.renderPostText()}
+        {this.renderSourceButton()}
+        <Text style={styles.headerPostDetailsLine}>
+          Posted by {this.props.post.by} | {this.props.post.score} Points
+        </Text>
+        <View style={styles.separator}/>
+        <Text style={styles.headerCommentTitle}>
+          {this.props.post.descendants} Comments:
+        </Text>
+      </View>
+    )
+  }
+
+  renderPostText () {
+    if(!this.props.post.text){
+      return null;
+    }
+    return(
+      <Text style={styles.headerPostText}>
+        {this.fixPostText(this.props.post.text)}
+      </Text>
+    )
+  }
+
+  renderSourceButton () {
+    if(!this.props.post.url){
+      return null;
+    }
+    return(
+      <TouchableHighlight onPress={() => this.pushSourceWebpage()}
+                          underlayColor='#F6F6EF'>
+        <Text style={styles.headerSourceLabel}>
+          (Source)
+        </Text>
+      </TouchableHighlight>
+    )
+  }
+
+  listViewOnRefresh (page, callback) {
+    if(!this.props.post.kids){
+      callback([], {allLoaded: true})
+    }
+    else if(page!=1){
+      this.fetchCommentsUsingKids(this.props.post.kids, this.state.lastIndex, 3, callback);
+    }
+    else{
+     this.fetchCommentsUsingKids(this.props.post.kids, 0, 3, callback);
     }
   }
 
-  onRefresh (page = 1, callback, options) {
-    this.props.onRefresh(page, callback)
+  fetchCommentsUsingKids (kids, startIndex, amountToAdd, callback) {
+    var rowsData = [];
+    var endIndex = (startIndex + amountToAdd) < kids.length ? (startIndex + amountToAdd) : kids.length;
+    function iterateAndFetch(){
+      if (startIndex < endIndex){
+        fetch(api.HN_ITEM_ENDPOINT+kids[startIndex]+".json")
+        .then((response) => response.json())
+        .then((item) => {
+            item.count = startIndex+1
+            if(!item.deleted){
+              rowsData.push(item)
+            }
+            startIndex++
+            iterateAndFetch()
+        })
+        .done()
+      }
+      else {
+        callback(rowsData, {allLoaded: endIndex==kids.length})
+        return
+      }
+    }
+    iterateAndFetch();
+    this.setState({lastIndex: endIndex})
   }
 
-  renderRow (row) {
-    return this.state.renderRow(row)
+  pushSourceWebpage () {
+    this.props.navigator.push({
+      id: 'Web',
+      title: this.props.post.title,
+      url: this.props.post.url
+    })
   }
 
-  render() {
-
+  fixPostText (str) {
+    return String(str).replace(/<p>/g, '\n\n')
+                  .replace(/&#x2F;/g, '/')
+                  .replace('<i>', '')
+                  .replace('</i>', '')
+                  .replace(/&#x27;/g, '\'')
+                  .replace(/&quot;/g, '\"')
+                  .replace(/<a\s+(?:[^>]*?\s+)?href="([^"]*)" rel="nofollow">(.*)?<\/a>/g, "$1")
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+      flex: 1
   },
-  navbarSpace: {
-    height: (Platform.OS !== 'android') ? 64 : 0,
+  toolbar: {
+    height: 56,
+    backgroundColor: '#FF6600'
   },
-  rowContainer: {
-    paddingRight: 15,
+  headerContainer: {
+    flex:1,
+    backgroundColor: '#F6F6EF',
+    flexDirection: 'column',
+    paddingRight: 10,
     paddingLeft: 10,
-    flexDirection: 'row'
   },
-  paginationView: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 20,
-    paddingBottom: 20
+  headerTitle:{
+    fontSize: 20,
+    textAlign: 'left',
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#FF6600',
   },
-  loadMoreText: {
+  headerPostText:{
+    fontSize: 14,
+    marginBottom: 10,
+    color: '#000000'
+  },
+  headerSourceLabel:{
     fontSize: 15,
-    color: 'gray'
-  }
+    textAlign: 'left',
+    color: '#0089FF',
+    paddingBottom: 10,
+  },
+  headerPostDetailsLine: {
+    fontSize: 12,
+    marginBottom: 10,
+    color: 'gray',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC',
+  },
+  headerCommentTitle: {
+    color: 'gray',
+    marginTop: 10
+  },
 })
 
 export default Post
